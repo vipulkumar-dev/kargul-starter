b11f7290b85fb82a2d74d3a0c1a64afdf5799699
Vipul Kumar <email.coex@gmail.com>
2026-02-02 05:26:45 +0530

    style: adjust max-width for heading and paragraph elements in ExampleSection for improved responsiveness

--- components/example-section.tsx
+++ components/example-section.tsx
@@ -12,8 +12,8 @@ function ExampleSection() {
         <div className="max-w-global mx-auto">
           <div className="flex flex-col items-center gap-4">
             {/* Dont' add any font styles + color styles to p, h1, h2 ,h3 because i have already made them global.css */}
-            <h1 className="max-w-[15ch] text-center">Example Section</h1>
-            <p className="max-w-[40ch] text-center">
+            <h1 className="max-w-[10em] text-center">Example Section</h1>
+            <p className="max-w-[24em] text-center">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste,
               neque laboriosam id harum consequatur quidem dolorem aperiam sit
             </p>
